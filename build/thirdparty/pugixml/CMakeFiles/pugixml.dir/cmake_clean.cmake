a5598127b3ad801ef1679639ade58b3411cd4b42
Batu Hatipoğlu <batuhatipoglu1@gmail.com>
2024-01-15 15:23:20 +0300

    adding project files

---
+++ build/thirdparty/pugixml/CMakeFiles/pugixml.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "/Users/bhatipoglu/Udacity-Cpp-ND/CppND-Route-Planning-Project/lib/libpugixml.a"
+  "/Users/bhatipoglu/Udacity-Cpp-ND/CppND-Route-Planning-Project/lib/libpugixml.pdb"
+  "CMakeFiles/pugixml.dir/src/pugixml.cpp.o"
+  "CMakeFiles/pugixml.dir/src/pugixml.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pugixml.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/thirdparty/pugixml/CMakeFiles/pugixml.dir/cmake_clean_target.cmake
@@ -0,0 +1,3 @@
+file(REMOVE_RECURSE
+  "/Users/bhatipoglu/Udacity-Cpp-ND/CppND-Route-Planning-Project/lib/libpugixml.a"
+)
