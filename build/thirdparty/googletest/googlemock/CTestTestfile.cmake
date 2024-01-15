ca2175cac6641f09389bd330f56b7cdc5c740718
Batu Hatipoğlu <batuhatipoglu1@gmail.com>
2024-01-15 15:47:34 +0300

    Delete unnecessary build files and dependencies

--- build/thirdparty/googletest/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/bhatipoglu/Udacity-Cpp-ND/CppND-Route-Planning-Project/thirdparty/googletest/googlemock
-# Build directory: /Users/bhatipoglu/Udacity-Cpp-ND/CppND-Route-Planning-Project/build/thirdparty/googletest/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
--- build/thirdparty/pugixml/CMakeFiles/pugixml.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "/Users/bhatipoglu/Udacity-Cpp-ND/CppND-Route-Planning-Project/lib/libpugixml.a"
-)
