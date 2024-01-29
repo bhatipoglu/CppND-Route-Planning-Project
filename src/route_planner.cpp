72721e9985536b8f89c9c6206f721ba0edbc1fd2
Batu Hatipoğlu <batuhatipoglu1@gmail.com>
2024-01-29 09:26:41 +0300

    Added detailed explanations for functions and classes in route_planner.cpp

--- src/route_planner.cpp
+++ src/route_planner.cpp
@@ -1,38 +1,57 @@
 #include "route_planner.h"
 #include <algorithm>
 
+/**
+ * @brief Constructs a RoutePlanner object.
+ * 
+ * @param model The RouteModel object representing the map.
+ * @param start_x The x-coordinate of the starting point.
+ * @param start_y The y-coordinate of the starting point.
+ * @param end_x The x-coordinate of the ending point.
+ * @param end_y The y-coordinate of the ending point.
+ */
+/**
+ * @brief Constructs a RoutePlanner object.
+ * 
+ * This constructor initializes a RoutePlanner object with the given parameters.
+ * It converts the input coordinates to percentages and uses the m_Model.FindClosestNode method
+ * to find the closest nodes to the starting and ending coordinates. The found nodes are stored
+ * in the RoutePlanner's start_node and end_node attributes.
+ * 
+ * @param model The RouteModel object.
+ * @param start_x The x-coordinate of the starting point.
+ * @param start_y The y-coordinate of the starting point.
+ * @param end_x The x-coordinate of the ending point.
+ * @param end_y The y-coordinate of the ending point.
+ */
 RoutePlanner::RoutePlanner(RouteModel &model, float start_x, float start_y, float end_x, float end_y): m_Model(model) {
     // Convert inputs to percentage:
     start_x *= 0.01;
     start_y *= 0.01;
     end_x *= 0.01;
     end_y *= 0.01;
-
-    // TODO 2: Use the m_Model.FindClosestNode method to find the closest nodes to the starting and ending coordinates.
-    // Store the nodes you find in the RoutePlanner's start_node and end_node attributes.
-    
+    // Find the closest nodes to the starting and ending coordinates
     start_node = &m_Model.FindClosestNode(start_x, start_y);
     end_node = &m_Model.FindClosestNode(end_x, end_y);
 }
 
-
-// TODO 3: Implement the CalculateHValue method.
-// Tips:
-// - You can use the distance to the end_node for the h value.
-// - Node objects have a distance method to determine the distance to another node.
-
+/**
+ * Calculates the heuristic value (H value) for a given node.
+ * The H value is the straight-line distance between the given node and the end node.
+ *
+ * @param node A pointer to the node for which the H value needs to be calculated.
+ * @return The calculated H value.
+ */
 float RoutePlanner::CalculateHValue(RouteModel::Node const *node) {
     return node->distance(*end_node);
 }
 
 
-// TODO 4: Complete the AddNeighbors method to expand the current node by adding all unvisited neighbors to the open list.
-// Tips:
-// - Use the FindNeighbors() method of the current_node to populate current_node.neighbors vector with all the neighbors.
-// - For each node in current_node.neighbors, set the parent, the h_value, the g_value. 
-// - Use CalculateHValue below to implement the h-Value calculation.
-// - For each node in current_node.neighbors, add the neighbor to open_list and set the node's visited attribute to true.
-
+/**
+ * Adds neighboring nodes to the open list and updates their attributes.
+ * 
+ * @param current_node A pointer to the current node.
+ */
 void RoutePlanner::AddNeighbors(RouteModel::Node *current_node) {
     // Populate neighbors vector for the current node
     current_node->FindNeighbors();
@@ -56,13 +75,13 @@ void RoutePlanner::AddNeighbors(RouteModel::Node *current_node) {
 }
 
 
-// TODO 5: Complete the NextNode method to sort the open list and return the next node.
-// Tips:
-// - Sort the open_list according to the sum of the h value and g value.
-// - Create a pointer to the node in the list with the lowest sum.
-// - Remove that node from the open_list.
-// - Return the pointer.
-
+/**
+ * @brief Returns the next node in the route.
+ * 
+ * This function is used to retrieve the next node in the route that needs to be visited.
+ * 
+ * @return A pointer to the next node in the route.
+ */
 RouteModel::Node *RoutePlanner::NextNode() {
     // Sort the open_list according to the sum of the h value and g value
     std::sort(open_list.begin(), open_list.end(), [](const auto &a, const auto &b) {
@@ -79,16 +98,12 @@ RouteModel::Node *RoutePlanner::NextNode() {
     return lowest_sum_node;
 }
 
-
-// TODO 6: Complete the ConstructFinalPath method to return the final path found from your A* search.
-// Tips:
-// - This method should take the current (final) node as an argument and iteratively follow the 
-//   chain of parents of nodes until the starting node is found.
-// - For each node in the chain, add the distance from the node to its parent to the distance variable.
-// - The returned vector should be in the correct order: the start node should be the first element
-//   of the vector, the end node should be the last element.
-
-    // TODO: Implement your solution here.
+/**
+ * @brief Represents a vector of RouteModel::Node objects.
+ * 
+ * This vector is used to store the nodes that make up the final path in the route planner.
+ * Each element in the vector represents a node in the path.
+ */
 std::vector<RouteModel::Node> RoutePlanner::ConstructFinalPath(RouteModel::Node *current_node) {
     // Create path_found vector
     distance = 0.0f;
@@ -113,13 +128,9 @@ std::vector<RouteModel::Node> RoutePlanner::ConstructFinalPath(RouteModel::Node 
     return path_found;
 }
 
-// TODO 7: Write the A* Search algorithm here.
-// Tips:
-// - Use the AddNeighbors method to add all of the neighbors of the current node to the open_list.
-// - Use the NextNode() method to sort the open_list and return the next node.
-// - When the search has reached the end_node, use the ConstructFinalPath method to return the final path that was found.
-// - Store the final path in the m_Model.path attribute before the method exits. This path will then be displayed on the map tile.
-
+/**
+ * Performs the A* search algorithm to find the shortest path from the start node to the end node.
+ */
 void RoutePlanner::AStarSearch() {
     RouteModel::Node *current_node = nullptr;
 
